96f5d0a2c5086934e6f1661c567847520ad7d94a
SujiBoii <kerem.kuecuek@hs-bochum.de>
2020-04-10 18:45:40 +0200

    need to work on live data fragment

--- app/src/main/java/com/example/datensammler2020/View/LiveData/LiveDataFragment.kt
+++ app/src/main/java/com/example/datensammler2020/View/LiveData/LiveDataFragment.kt
@@ -9,14 +9,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.datensammler2020.R
+import com.example.datensammler2020.SharedPreferences.SharedPreferences
+import kotlinx.android.synthetic.main.fragment_live_data.*
 
 class LiveDataFragment : Fragment(), SensorEventListener {
 
     private var mSensorManager: SensorManager? = null
     private var mAccelerometer: Sensor? = null
     private var resume = true;
+    private val sharedPreferences by lazy { SharedPreferences(requireContext()) }
 
 
     override fun onCreateView(
@@ -29,6 +33,10 @@ class LiveDataFragment : Fragment(), SensorEventListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         mSensorManager = context?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
     }
@@ -36,6 +44,11 @@ class LiveDataFragment : Fragment(), SensorEventListener {
     override fun onResume() {
         super.onResume()
         mSensorManager?.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mSensorManager?.unregisterListener(this, mAccelerometer)
     }
 
 
@@ -50,8 +63,20 @@ class LiveDataFragment : Fragment(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?) {
         if (event != null && resume) {
             if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                accelerometerTv(event)
             }
         }
+    }
+
+    private fun accelerometerTv(event: SensorEvent){
+        if (sharedPreferences.isAccelerometerActivated()){
+            requireActivity().findViewById<TextView>(R.id.accelerometerTvX).text = event.values[0].toString()
+            requireActivity().findViewById<TextView>(R.id.accelerometerTvY).text = event.values[1].toString()
+            requireActivity().findViewById<TextView>(R.id.accelerometerTvZ).text = event.values[2].toString()
+        } else{
+            return
+        }
+
     }
 
 }
