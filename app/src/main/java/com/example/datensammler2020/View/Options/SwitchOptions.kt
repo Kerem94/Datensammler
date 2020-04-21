b6daa63c22cb3fe655820685854f5b924b3de94b
Kücük, Kerem <kerem.kuecuek@gdata.de>
2020-04-21 14:33:16 +0200

    Fixed codesmells

--- app/src/main/java/com/example/datensammler2020/View/Options/SwitchOptions.kt
+++ app/src/main/java/com/example/datensammler2020/View/Options/SwitchOptions.kt
@@ -21,66 +21,38 @@ class SwitchOptions(private val view: View, private val sharedPreferences: Share
 
     private fun accelerometerListener(){
         view.accelerometerSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked){
                 sharedPreferences.setAccelerometerActivated(isChecked)
-            } else{
-                sharedPreferences.setAccelerometerActivated(isChecked)
-            }
         }
     }
 
     private fun gyroListener(){
         view.gyroscopeSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked){
                 sharedPreferences.setGyroActivated(isChecked)
-            } else{
-                sharedPreferences.setGyroActivated(isChecked)
-            }
         }
     }
     private fun gravityListener(){
         view.gravitySwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked){
                 sharedPreferences.setGravityActivated(isChecked)
-            } else{
-                sharedPreferences.setGravityActivated(isChecked)
-            }
         }
     }
     private fun linearAccelerationListener(){
         view.linearAccSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked){
                 sharedPreferences.setLinearAccelerationActivated(isChecked)
-            } else{
-                sharedPreferences.setLinearAccelerationActivated(isChecked)
-            }
         }
     }
     private fun proximityListener(){
         view.proximitySwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked){
                 sharedPreferences.setProximityActivated(isChecked)
-            } else{
-                sharedPreferences.setProximityActivated(isChecked)
-            }
         }
     }
     private fun lightListener(){
         view.lightSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked){
                 sharedPreferences.setLightActivated(isChecked)
-            } else{
-                sharedPreferences.setLightActivated(isChecked)
-            }
         }
     }
     private fun magneticFieldListener(){
         view.magnetFieldSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked){
                 sharedPreferences.setMagneticFieldActivated(isChecked)
-            } else{
-                sharedPreferences.setMagneticFieldActivated(isChecked)
-            }
         }
     }
 }
